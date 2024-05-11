e159d14ff2a8e7af8015cce1c5473cff71c97f5d
abhishekkumarchaudhary1 <abhishek.chaudhary33@gmail.com>
2024-05-11 16:54:08 +0530

    setting up redux toolkit in our project through store authslice>reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-
 import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
-  return (
-    <>
-      <h1>Making a blog app</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+
+    .finally(() => setLoading(false))
+  }, [])
+
+ return !loading ? (
+  <div className='min-h-screen flex flex-wrap text-center content-between bg-gray-400'>
+    <div className='w-full block text-center'>
+      <Header />
+      <main>
+        {/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+  </div>
+
+ ) : null
 }
 
 export default App
